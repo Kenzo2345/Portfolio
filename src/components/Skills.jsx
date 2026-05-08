7eabd6bd994b8fc0f05e8e6891b2d69fde2b88a3
Taibi Zineb <taibizineb96@gmail.com>
2026-05-08 12:06:19 +0100

    Skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -77,7 +77,7 @@ export default function Skills() {
         </div>
 
         {/* HPC */}
-        <div className="bg-white dark:bg-[#1e293b]
+        <div className="bg-gradient-to-br from-white to-blue-100 dark:bg-[#1e293b]
         rounded-2xl p-8 shadow-lg hover:shadow-2xl
         hover:-translate-y-2 transition duration-300">
 
@@ -98,8 +98,7 @@ export default function Skills() {
             ].map((skill) => (
               <span
                 key={skill}
-                className="bg-slate-100 dark:bg-slate-800
-                text-slate-700 dark:text-slate-300
+                className="bg-blue-100 text-blue-600 
                 px-4 py-2 rounded-full text-sm font-medium"
               >
                 {skill}
@@ -109,7 +108,7 @@ export default function Skills() {
         </div>
 
         {/* Soft Skills */}
-        <div className="md:col-span-2 bg-white dark:bg-[#1e293b]
+        <div className="md:col-span-2 bg-gradient-to-br from-white to-blue-100 dark:bg-[#1e293b]
         rounded-2xl p-8 shadow-lg hover:shadow-2xl
         hover:-translate-y-2 transition duration-300">
 
@@ -128,8 +127,8 @@ export default function Skills() {
             ].map((skill) => (
               <span
                 key={skill}
-                className="bg-blue-100 dark:bg-blue-900/40
-                text-blue-600 dark:text-blue-300
+                className="bg-blue-100 
+                text-blue-600 
                 px-4 py-2 rounded-full text-sm font-medium"
               >
                 {skill}
