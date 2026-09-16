73da417000378d7c02a1b9640cb2a07fcda54618
Kenzo2345 <kenzataibi11@gmail.com>
2026-09-16 14:45:17 +0200

    add BMCE experience and update dates

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,7 +7,7 @@ function Experience() {
         {/* Experience 1 */}
           <div className="bg-gradient-to-br from-white to-blue-100 dark:bg-[#1e293b] rounded-2xl p-6 shadow-lg hover:shadow-2xl hover:-translate-y-2 transition duration-300">
             <h3 className="text-2xl font-semibold mb-3 text-blue-500"> Détection de Bugs Numériques via IA</h3>
-            <p className="text-sm text-gray-500 dark:text-gray-400 mb-4"> Espace Dev UPVD • Avril – Mai 2026</p>
+            <p className="text-sm text-gray-500 dark:text-gray-400 mb-4"> Espace Dev UPVD • Avril – Juillet 2026</p>
             <p className="text-gray-600 leading-7"> Développement d’un prototype multi-agents basé sur des modèles de LLM afin d’identifier et analyser des bugs numériques. Travail autour de l’IA générative, l’analyse automatisée de code, la détection d’erreurs de calcul et l’optimisation d’outils d’automatisation.</p>
             <div className="mt-5 flex flex-wrap gap-2">
               <span className="bg-blue-100 text-blue-500 px-3 py-1 rounded-full text-xs"> IA Générative</span>
@@ -15,10 +15,24 @@ function Experience() {
               <span className="bg-blue-100 text-blue-500 px-3 py-1 rounded-full text-xs"> Analyse de Code </span>
             </div>
           </div>
-        {/* Experience 2 */}
+          {/* Experience 2 */}
+          <div className="bg-gradient-to-br from-white to-blue-100 dark:bg-[#1e293b] rounded-2xl p-6 shadow-lg hover:shadow-2xl hover:-translate-y-2 transition duration-300">
+            <h3 className="text-2xl font-semibold mb-3 text-blue-500"> Quantization et Distillation de Modèle NLP</h3>
+            <p className="text-sm text-gray-500 dark:text-gray-400 mb-4"> BMCE • Juillet – Aout 2025</p>
+            <p className="text-gray-600 leading-7"> Réduction de la taille et latence d'un modèle BERT via quantization et distillation.
+Benchmark comparative de performance : 80% de réduction de taille, 3x speedup, avec
+conservation de l'accuracy. Déploiement et tests sur CPU.
+</p>
+            <div className="mt-5 flex flex-wrap gap-2">
+              <span className="bg-blue-100 text-blue-500 px-3 py-1 rounded-full text-xs"> IA</span>
+              <span className="bg-blue-100 text-blue-500 px-3 py-1 rounded-full text-xs"> Optimisation</span>
+              <span className="bg-blue-100 text-blue-500 px-3 py-1 rounded-full text-xs"> Quantization </span>
+            </div>
+          </div>
+        {/* Experience 3 */}
         <div className="bg-gradient-to-br from-white to-blue-100 dark:bg-[#1e293b] rounded-2xl p-6 shadow-lg hover:shadow-2xl hover:-translate-y-2 transition duration-300">
           <h3 className="text-2xl font-semibold mb-3 text-blue-500"> Développement Web Fullstack</h3>
-          <p className="text-sm text-gray-500 mb-4"> MTDS • Avril – Mai 2024</p>
+          <p className="text-sm text-gray-500 mb-4"> MTDS • Avril – Juillet 2024</p>
           <p className="text-gray-600  leading-7"> Conception, développement fullstack et mise en ligne du site institutionnel mtds.com à partir d’une maquette graphique.
           Intégration responsive de l’interface utilisateur et développement des différentes pages du site.</p>
           <div className="mt-5 flex flex-wrap gap-2">
